7530730862dbab0085c683e06caf718de145db17
erismat1mus <73518407+erismat1mus@users.noreply.github.com>
2026-04-08 12:19:06 +0300

    Add files via upload

---
+++ CreatePrimitives.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CreatePrimitives : MonoBehaviour
+{
+    void Start()
+    {
+        // Создаётся шесть кубов в определенных точках пространства для сборки фигуры в виде "Стива" из Minecraft
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0, 0, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(-0.5f, 1, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0.5f, 1, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0.5f, 2, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(-0.5f, 2, 0);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0, 3, 0);
+    }
+}
---
+++ Crickets.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Crickets : MonoBehaviour
+{
+    void Update()
+    {
+        Debug.Log("crickets"); // Выводит слово «crickets» в консоль каждый кадр
+    }
+}
